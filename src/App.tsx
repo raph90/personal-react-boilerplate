94e2a6301467e471edec187ed0a89f5af71d1f56
Raph <raph37heth@gmail.com>
2021-12-17 19:17:32 +0000

    some more stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { FC } from 'react';
-import { Slider } from 'antd';
+import { Button } from 'antd';
 import Hello from './components/Hello';
 
 const App: FC = () => {
   return (
     <>
-      <Slider className="bg-black" defaultValue={30} />
-      <h3 className="text-slate-50"> Welcome to React Boilerplate</h3>
+      <Button className='color-white'>hello there</Button>
+      <h3 className='text-slate-50'> Welcome to React Boilerplate</h3>
       <Hello />
     </>
   );
